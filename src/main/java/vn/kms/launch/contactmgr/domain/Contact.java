9c59e71cd74645d1d5d6f6ec84acfb60d9eb2e43
ThoaiTran <thoaitran@kms-techonology.com>
2015-04-08 17:25:01 +0700

    Create Entity and Database

---
+++ src/main/java/vn/kms/launch/contactmgr/domain/Contact.java
@@ -0,0 +1,136 @@
+package vn.kms.launch.contactmgr.domain;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CONTACT")
+public class Contact {
+
+	@Id
+	@Column(name = "ID")
+	private int id;
+	@Column(name = "FIRST_NAME")
+	private String firstName;
+	@Column(name = "MIDDLE_NAME")
+	private String middleName;
+	@Column(name = "LAST_NAME")
+	private String lastname;
+	@Column(name = "EMAIL")
+	private String email;
+	
+	@Lob
+	@Column(name = "PHOTO")
+	private String photo;
+	@Column(name = "MOBILE", columnDefinition="char(15)")
+	private String mobile;
+	
+	@Column(name = "GENDER")
+	private String gender;
+	
+	@Column(name = "BIRTHDAY", columnDefinition="date")
+	private Date birthday;
+	
+	@Embedded
+	private Home home;
+
+	@Embedded
+	private Work work;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public Home getHome() {
+		return home;
+	}
+
+	public void setHome(Home home) {
+		this.home = home;
+	}
+
+	public Work getWork() {
+		return work;
+	}
+
+	public void setWork(Work work) {
+		this.work = work;
+	}
+}
