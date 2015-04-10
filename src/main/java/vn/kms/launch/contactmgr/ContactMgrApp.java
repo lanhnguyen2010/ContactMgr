4c9308cb3b30dbfdbbb7492f1cf19922d407a4ed
Trung Nguyen <trungnguyen@kms-technology.com>
2015-04-10 15:30:06 +0700

    Remove unnecessary annotations

--- src/main/java/vn/kms/launch/contactmgr/ContactMgrApp.java
+++ src/main/java/vn/kms/launch/contactmgr/ContactMgrApp.java
@@ -1,23 +1,17 @@
 package vn.kms.launch.contactmgr;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 
 @SpringBootApplication
-@ComponentScan
-@EnableAutoConfiguration
-@EnableTransactionManagement
 public class ContactMgrApp {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(ContactMgrApp.class);
-		
+
 
 		SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(
 				args);
