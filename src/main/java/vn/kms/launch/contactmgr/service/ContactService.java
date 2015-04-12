31341c4f380c8167e67ae11f6dba55a65d3871e8
Lanh Nguyen <lanhnguyen@kms-technology.com>
2015-04-13 01:56:45 +0700

    [S06][DEV] Search Service update final build

--- src/main/java/vn/kms/launch/contactmgr/service/ContactService.java
+++ src/main/java/vn/kms/launch/contactmgr/service/ContactService.java
@@ -2,180 +2,264 @@ package vn.kms.launch.contactmgr.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.validation.Valid;
 
-import org.hibernate.Query;
-import org.hibernate.Session;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-
-import scala.annotation.meta.setter;
-import scala.util.parsing.json.JSON;
+import vn.kms.launch.contactmgr.domain.contact.Company;
 import vn.kms.launch.contactmgr.domain.contact.Contact;
+import vn.kms.launch.contactmgr.domain.contact.Work;
+import vn.kms.launch.contactmgr.domain.search.ContactSearchCriteria;
+import vn.kms.launch.contactmgr.repository.CompanyRepository;
 import vn.kms.launch.contactmgr.repository.ContactRepository;
 
 @Service
+@Transactional(readOnly = true)
 public class ContactService {
+
+	private static final String C_LAST_NAME = "c.lastName";
+
+	private static final String C_MIDDLE_NAME = "c.middleName";
+
+	private static final String C_WORK_COMPANY_NAME = "c.work.company.name";
+
+	private static final String C_WORK_DEPARTMENT = "c.work.department";
+
+	private static final String C_WORK_TITLE = "c.work.title";
+
+	private static final String C_EMAIL = "c.email";
+
+	private static final String C_MOBILE = "c.mobile";
+
+	private static final String C_FIRST_NAME = "c.firstName";
+
+	private static final String SELECT_C_FROM_CONTACT_C = "select c from Contact c ";
+
+	private static final String SELECT_COUNT_FROM_CONTACT_C = "select COUNT(c) from Contact c ";
+
 	@Autowired
 	private ContactRepository contactRepo;
-	
+
+	@Autowired
+	private CompanyRepository companyRepo;
+
+	@PersistenceContext
 	private EntityManager em;
-	private Session session;
 
 	/**
 	 * Get a contact by id
-	 * @param id is ID of the contact we will get.
+	 * 
+	 * @param id
+	 *            is ID of the contact we will get.
 	 * @return a contact if found and null if not found.
 	 */
+	@Transactional
 	public Contact getContact(int id) {
 		return contactRepo.findOne(id);
 	}
-	
-	public static class ContactSearchCriteria {
-        private String name;
-        private String mobile;
-        private String email;
-        private String jobTitle;
-        private String department;
-        private String company;
-        
-        private void setName(String name){
-            this.name = name;
-        }
-        
-        private void setMobile(String mobile){
-            this.mobile = mobile;
-        }
-        
-        private void setEmail(String email){
-            this.email = email;
-        }
-        
-        private void setJobTitle(String jobTitle){
-            this.jobTitle = jobTitle;
-        }
-        
-        private void setDepartment(String department){
-            this.department = department;
-        }
-        
-        private void setCompany(String company) {
-            this.company = company;
-        }
-    }
 
-    public List<Contact> searchContacts(ContactSearchCriteria criteria) {
-    	
-        StringBuilder queryString = null;
-        queryString.append("select c from Contact c where ");        		
-        if (!StringUtils.isEmpty(criteria.name)){ 
-            if (criteria.name.contains("\\*")){
-                criteria.name.replace("\\*", "\\%");
-                queryString.append("lower(c.firstName) like :name "
-                           + "or lower(c.middleName) like :name "
-                           + "or lower(c.lastName) like :name ");
-    	    }else{
-                queryString.append("lower(c.firstName) =:name ");
-                queryString.append("or lower(c.middleName) =:name ");
-                queryString.append("or lower(c.lastName) =:name ");
-    	        
-    	    }
-        }
-        
-        if(queryString.length() != ("select c from Contact c where ").length()){
-          queryString.append(" and ");
-        }
-        if (!StringUtils.isEmpty(criteria.mobile)){
-            if (criteria.mobile.contains("\\*")){
-                criteria.mobile.replace("\\*", "\\%");
-                queryString.append("lower(c.mobile) like :mobile ");
-            }else {
-                queryString.append("lower(c.mobile) =:mobile ");
-            }
-        }
-        
-        if(queryString.length() != ("select c from Contact c where ").length()){
-            queryString.append(" and ");
-          }
-        if (!StringUtils.isEmpty(criteria.email)){
-            if (criteria.email.contains("\\*")){
-                criteria.email.replace("\\*", "\\%");
-                queryString.append("lower(c.email) like :email ");
-            }else {
-                queryString.append("lower(c.email) =:email ");
-            }
-        }
-        
-        if(queryString.length() != ("select c from Contact c where ").length()){
-            queryString.append(" and ");
-          }
-        if (!StringUtils.isEmpty(criteria.jobTitle)){
-            if (criteria.jobTitle.contains("\\*")){
-                criteria.jobTitle.replace("\\*", "\\%");
-                queryString.append("lower(c.work.title) like :jobTitle ");
-            }else {
-                queryString.append("lower(c.work.title) =:jobTitle ");
-            }
-        }
-        
-        if(queryString.length() != ("select c from Contact c where ").length()){
-            queryString.append(" and ");
-          }
-        if (!StringUtils.isEmpty(criteria.department)){
-            if (criteria.department.contains("\\*")){
-                criteria.department.replace("\\*", "\\%");
-                queryString.append("lower(c.work.department) like :department ");
-            }else {
-                queryString.append("lower(c.work.department) =:department ");
-            }
-        }
-        
-        if(queryString.length() != ("select c from Contact c where ").length()){
-            queryString.append(" and ");
-          }
-        if (!StringUtils.isEmpty(criteria.company)){
-            if (criteria.company.contains("\\*")){
-                criteria.company.replace("\\*", "\\%");
-                queryString.append("lower(c.work.company.name) like :company ");
-            }else {
-                queryString.append("lower(c.work.company.name) =:company ");
-            }
-        }
-        TypedQuery<Contact> query = em.createQuery(queryString.toString(),Contact.class).setParameter("name", criteria.name)
-        		                                                    .setParameter("email", criteria.email)
-        		                                                    .setParameter("mobile", criteria.mobile)
-        		                                                    .setParameter("jobTitle",criteria.jobTitle)
-        		                                                    .setParameter("company", criteria.company)
-        		                                                    .setParameter("department", criteria.department);
-        List<Contact> contacts = query.getResultList();
+	@Transactional
+	public Contact saveContact(@Valid Contact contact) {
 
-        
-
-
-
-		
-		
-		
-		return contacts;
-		
-	}
-	
-	public int countContacts(ContactSearchCriteria criteria, int pageIndex, int pageSize) {
-		if (!StringUtils.isEmpty(criteria.name)) {
-			
+		if (contact == null) {
+			return null;
 		}
-		int total = 0;
-		return total;
+
+		Work work = contact.getWork();
+
+		if (work != null) {
+			Integer companyId = contact.getWork().getCompanyId();
+			Company company = contact.getWork().getCompany();
+
+			if (companyId != null) {
+				Company com = getCompany(companyId);
+				if (com == null) {
+					return null;
+				}
+			}
+			if (company != null) {
+				saveCompany(company);
+			}
+		}
+
+		return contactRepo.save(contact);
 	}
 
-        
+	@Transactional
+	public Company getCompany(Integer id) {
+		return companyRepo.findOne(id);
+	}
+
+	@Transactional
+	public List<Company> getAllCompany() {
+		return companyRepo.findAll();
+	}
+
+	@Transactional
+	public Company saveCompany(@Valid Company company) {
+
+		if (company == null) {
+			return null;
+		}
+
+		return companyRepo.save(company);
+	}
+
+	/**
+	 * Service form function delete
+	 * 
+	 * @param ids
+	 * @return
+	 */
+	@Transactional
+	public int deleteContacts(Integer... ids) {
+		return contactRepo.deleteContacts(ids);
+	}
+
+	/**
+	 * Set search criteria to HashMap<K, V> , K - column name, V - criteria value
+	 * @param criteria
+	 * @return 
+	 */
+	public LinkedHashMap<String, String> setSearchParam(ContactSearchCriteria criteria) {
+		LinkedHashMap<String, String> params = new LinkedHashMap<String, String>();
+
+		if (!StringUtils.isEmpty(criteria.getName())) {
+			params.put(C_FIRST_NAME, criteria.getName());
+			params.put(C_MIDDLE_NAME, criteria.getName());
+			params.put(C_LAST_NAME, criteria.getName());
+		}
+		System.out.println("name: " + criteria.getName());
+
+		if (!StringUtils.isEmpty(criteria.getMobile())) {
+			params.put(C_MOBILE, criteria.getMobile());
+		}
+
+		if (!StringUtils.isEmpty(criteria.getEmail())) {
+			params.put(C_EMAIL, criteria.getEmail());
+		}
+
+		if (!StringUtils.isEmpty(criteria.getJobTitle())) {
+			params.put(C_WORK_TITLE, criteria.getJobTitle());
+		}
+
+		if (!StringUtils.isEmpty(criteria.getDepartment())) {
+			params.put(C_WORK_DEPARTMENT, criteria.getDepartment());
+		}
+
+		if (!StringUtils.isEmpty(criteria.getCompany())) {
+			params.put(C_WORK_COMPANY_NAME, criteria.getCompany());
+		}
+		return params;
+	}
 	
+	/**
+	 * Get real page selected from request
+	 * @param page
+	 * @param total
+	 * @param pageSize
+	 * @return 0 if page < 1
+	 *         maxPage if page > maxPage
+	 *         page - 1 if others
+	 */
+	private int getRealPageSelected(int page, int total, int pageSize) {
+		page = page - 1;
+		int maxPage = (int) Math.ceil(total / (double) pageSize);
+		if (page <= 0 || maxPage <= 0) {
+			page = 0;
+		} else if (page >= maxPage) {
+			page = maxPage - 1;
+		}
+		return page;
+	}
+
+	/**
+	 * Search contact with given criterias
+	 * 
+	 * @param criteria
+	 * @param page
+	 * @param pageSize
+	 * @return
+	 */
+	public HashMap<String, Object> searchContacts(ContactSearchCriteria criteria, int page, int pageSize) {
+
+		StringBuilder queryString = new StringBuilder(SELECT_C_FROM_CONTACT_C);
+		StringBuilder queryCountString = new StringBuilder(SELECT_COUNT_FROM_CONTACT_C);
+
+		LinkedHashMap<String, String> paramsCriteria = setSearchParam(criteria);
+
+		Set<String> columnSet = paramsCriteria.keySet();
+		int paramPos = 1;
+		if (columnSet != null && !columnSet.isEmpty()) {
+			StringBuilder whereClause = new StringBuilder("where ");
+			String operator = "";
+			if (!StringUtils.isEmpty(criteria.getName())) {
+				whereClause.append("(");
+			}
+			for (String column : columnSet) {
+				whereClause.append(operator);
+				whereClause.append(column);
+				if (paramsCriteria.get(column).contains("*")) {
+					whereClause.append(" like ?" + paramPos);
+				} else {
+					whereClause.append(" =?" + paramPos);
+				}
+				if (!StringUtils.isEmpty(criteria.getName()) && paramPos < 3) {
+					operator = " OR ";
+					
+				} else {
+					if (paramPos == 3) {
+						whereClause.append(")");
+					}
+					operator = " AND ";
+				}
+				paramPos++;
+			}
+			queryString.append(whereClause);
+			queryCountString.append(whereClause);
+		}
+
+		TypedQuery<Contact> query = em.createQuery(queryString.toString(), Contact.class);
+		TypedQuery<Long> queryCount = em.createQuery(queryCountString.toString(), Long.class);
+
+		paramPos = 1;
+		for (String column : columnSet) {
+			if (column.equals(C_FIRST_NAME) || column.equals(C_MIDDLE_NAME) || column.equals(C_LAST_NAME) 
+					|| column.equals(C_MOBILE) || column.equals(C_EMAIL)
+					|| column.equals(C_WORK_TITLE) || column.equals(C_WORK_DEPARTMENT)
+					|| column.equals(C_WORK_COMPANY_NAME)) {
+				query.setParameter(paramPos, paramsCriteria.get(column).replace("*", "%"));
+				queryCount.setParameter(paramPos, paramsCriteria.get(column).replace("*", "%"));
+			}
+			paramPos++;
+		}
+		
+		int realPage = getRealPageSelected(page, queryCount.getSingleResult().intValue(), pageSize);
+		query.setFirstResult(realPage * pageSize).setMaxResults((realPage + 1) * pageSize - 1);
+		List<Contact> contacts = query.getResultList();
+		
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		result.put("searchCriteria", criteria);
+		result.put("page", page);
+		result.put("pageSize", pageSize);
+		result.put("totalItem", queryCount.getSingleResult().intValue());
+		result.put("data", contacts);
+
+		return result;
+	}
 }
--- src/main/java/vn/kms/launch/contactmgr/web/controller/ContactController.java
+++ src/main/java/vn/kms/launch/contactmgr/web/controller/ContactController.java
@@ -1,13 +1,17 @@
 package vn.kms.launch.contactmgr.web.controller;
 
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 import java.util.HashMap;
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JsonJsonParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,23 +20,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-
+import vn.kms.launch.contactmgr.domain.contact.Company;
 import vn.kms.launch.contactmgr.domain.contact.Contact;
+import vn.kms.launch.contactmgr.domain.contact.Work;
+import vn.kms.launch.contactmgr.domain.search.ContactSearchCriteria;
 import vn.kms.launch.contactmgr.service.ContactService;
-import vn.kms.launch.contactmgr.service.ContactService.ContactSearchCriteria;
 
 @RestController
 @RequestMapping(value = "/api/contacts")
 public class ContactController {
 	@Autowired
 	private ContactService contactService;
-	
+
 	/**
 	 * Get detail of an existing contact
+	 *
 	 * @param id is ID of the contact we need get.
 	 * @return "404 code" if not found or "200 code and data of contact"
 	 */
@@ -42,16 +44,89 @@ public class ContactController {
 		if (contact == null) {
 			return new ResponseEntity<Contact>(HttpStatus.NOT_FOUND);
 		}
-		
+
 		return new ResponseEntity<Contact>(contact, HttpStatus.OK);
 	}
-
 	
-    @RequestMapping(value = "/{id}",method = POST)
-    public List<Contact> searchContact(@PathVariable ContactSearchCriteria criteria){
-   	
-    	
-        return contactService.searchContacts(criteria);
-        
+	/**
+	 * Search contact by given criteria
+	 * @param page
+	 * @param pageSize
+	 * @param criteria
+	 * @return
+	 * @throws JSONException
+	 */
+    @RequestMapping(method = POST, value="/search")
+	public HashMap<String, Object> searchContact(@RequestParam ("page") int page,
+    		                                     @RequestParam (value="pageSize", defaultValue="10") int pageSize,
+    		                                     @RequestBody ContactSearchCriteria criteria) throws JSONException {
+        return contactService.searchContacts(criteria, page, pageSize);
     }
+
+    /**
+     *
+     * @param id
+     * @param contact
+     * @return
+     */
+    @RequestMapping(value = "/{id}", method = PUT)
+    public ResponseEntity<Contact> updateContact(@PathVariable int id, @RequestBody @Valid Contact contact) {
+		Contact returnContact = contactService.saveContact(contact);
+    	
+		if (returnContact == null) { 
+    		return new ResponseEntity<Contact>(returnContact, HttpStatus.BAD_REQUEST);
+    	}
+
+        return new ResponseEntity<Contact>(returnContact, HttpStatus.OK);
+    }
+
+	/**
+	 * Return 404 not found code if not contact associated to ID is not found
+	 * Return 200 success code if deleted successfully
+	 */
+	@RequestMapping(value = "/{contactId}", method = DELETE)
+	public ResponseEntity<Void> deleteContact(@PathVariable int contactId) {
+		
+		int deleteId = contactService.deleteContacts(contactId);
+		//receive  id with method deleteContact() from UI
+
+		if (deleteId == 0) {
+			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+	/**
+	 * Return 404 not found code if not contact associated to ID is not found
+	 * Return 200 success code if deleted successfully
+	 */
+	@RequestMapping(method = DELETE)
+	public ResponseEntity<Integer> deleteContacts(@RequestParam Integer... contactIds) {
+
+		int deleteId = contactService.deleteContacts(contactIds);
+		if (deleteId == 0) {
+			return new ResponseEntity<Integer>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<Integer>(deleteId,HttpStatus.OK);
+	}
+
+	/**
+	 * Create a new contact
+	 * @param contact
+	 * @return
+	 */
+	@RequestMapping(method = POST)
+	public ResponseEntity<Contact> create(@RequestBody @Valid Contact contact) {
+		
+		Contact returnContact = contactService.saveContact(contact);
+    	
+		if (returnContact == null) { 
+    		return new ResponseEntity<Contact>(returnContact, HttpStatus.BAD_REQUEST);
+    	}
+
+        return new ResponseEntity<Contact>(returnContact, HttpStatus.OK);
+	}
+	
 }
