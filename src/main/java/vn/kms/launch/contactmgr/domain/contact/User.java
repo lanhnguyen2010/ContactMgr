5734c0c0c6280c10e1af97f2b6e8ee86f9284886
thanhtuong <thanhtuong@kms-technology.com>
2015-04-14 11:50:37 +0700

    [S17]: Create user table and mapping to user entity

---
+++ src/main/java/vn/kms/launch/contactmgr/domain/contact/User.java
@@ -0,0 +1,87 @@
+package vn.kms.launch.contactmgr.domain.contact;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class User extends vn.kms.launch.contactmgr.domain.Entity {
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name="USERNAME")
+	private String userName;
+	
+	@Column(name="PASSWORD")
+	private String passWord;
+	
+	@Column(name="FIRST_NAME")
+	private String firstName;
+	
+	@Column(name="LAST_NAME")
+	private String lastName;
+	
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
+	@Column(name="EXPIRED_DATE")
+	private Date expiredDate;
+	
+	@Column(name="ACTIVE")
+	private int active;
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	public void setExpiredDate(Date expiredDate) {
+		this.expiredDate = expiredDate;
+	}
+	
+	public void setActive(int active) {
+		this.active = active;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public String getPassWord() {
+		return passWord;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public Role getRole() {
+		return role;
+	}
+	public int getActive() {
+		return active;
+	}
+}
