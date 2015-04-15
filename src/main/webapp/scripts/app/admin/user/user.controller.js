0d5742f08dd8f9c789f9410051206352dd9e713f
Trang Bien <user.name>
2015-04-15 19:02:17 +0700

    change design create user interface

--- src/main/webapp/scripts/app/admin/user/user.controller.js
+++ src/main/webapp/scripts/app/admin/user/user.controller.js
@@ -2,15 +2,21 @@
 
 angular.module('contactmgrApp')
     .controller('UserController', function($scope) {
-
-    	 $scope.data = {
+    	 $scope.user = {
                  username: '',
                  newPassword: '',
                  confirmNewPassword: '',
+                 assignCompany:'',
                  role: '',
                  firstName: '',
                  lastName: '',
                  expiredDate: '',
                  active: ''
              };
-    });
+    }).controller('DateController', ['$scope', function($scope) {
+        $scope.value = new Date();
+        $scope.saveUser=function(){
+        	console.log("User" + $scope.user.username);
+        	UserService.saveUser($scope.user)
+        }
+    }]);
