8d0c5c6ad29cf884f63a84d6e99e07fa64418a8f
thoong <thoong@kms-technology.com>
2015-04-15 18:26:54 +0700

    merge from master

--- src/main/java/vn/kms/launch/contactmgr/domain/contact/Language.java
+++
@@ -1,6 +0,0 @@
-package vn.kms.launch.contactmgr.domain.contact;
-
-public enum Language {
-	ENGLISH,
-	VIETNAMESE
-}
--- src/test/java/vn/kms/launch/contactmgr/service/SearchServiceTest.java
+++ src/test/java/vn/kms/launch/contactmgr/service/SearchServiceTest.java
@@ -1,60 +1,60 @@
-package vn.kms.launch.contactmgr.service;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import vn.kms.launch.contactmgr.ContactMgrApp;
-import vn.kms.launch.contactmgr.domain.contact.Contact;
-import vn.kms.launch.contactmgr.domain.search.ContactSearchCriteria;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ContactMgrApp.class)
-@WebAppConfiguration
-public class SearchServiceTest {
-	@Autowired
-	private ContactService contactService;
-	
-	
-
-	
-	@SuppressWarnings("deprecation")
-	public Contact readContactFile() throws JsonParseException, IOException{
-		Contact contact = new Contact();
-		JsonFactory jFactory = new JsonFactory();
-		System.out.println("read");
-		
-//		InputStream inputStream = getClass().getClassLoader().getResourceAsStream("/etc/contact.json");
-//		JsonParser jParser = jFactory.createJsonParser(inputStream);
-//		jParser.nextToken();
-//		System.out.println(jParser.getText());
-		ObjectMapper mapper = new ObjectMapper();
-		ContactSearchCriteria criteria = mapper.readValue(getClass().getClassLoader().getResourceAsStream("/etc/searchcriteria/searchcriteria1.json"), ContactSearchCriteria.class);
-		System.out.println("result"+criteria.getName());
-		
-		
-		
-		return contact;
-		
-	}
-	@Test
-	public void testContactService() throws JsonParseException, IOException{
-		Contact contact = readContactFile(); 
-		
-	}
-	
-
-}
+//package vn.kms.launch.contactmgr.service;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.io.InputStream;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//
+//import com.fasterxml.jackson.core.JsonFactory;
+//import com.fasterxml.jackson.core.JsonParseException;
+//import com.fasterxml.jackson.core.JsonParser;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//import vn.kms.launch.contactmgr.ContactMgrApp;
+//import vn.kms.launch.contactmgr.domain.contact.Contact;
+//import vn.kms.launch.contactmgr.domain.search.ContactSearchCriteria;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = ContactMgrApp.class)
+//@WebAppConfiguration
+//public class SearchServiceTest {
+//	@Autowired
+//	private ContactService contactService;
+//
+//
+//
+//
+//	@SuppressWarnings("deprecation")
+//	public Contact readContactFile() throws JsonParseException, IOException{
+//		Contact contact = new Contact();
+//		JsonFactory jFactory = new JsonFactory();
+//		System.out.println("read");
+//
+////		InputStream inputStream = getClass().getClassLoader().getResourceAsStream("/etc/contact.json");
+////		JsonParser jParser = jFactory.createJsonParser(inputStream);
+////		jParser.nextToken();
+////		System.out.println(jParser.getText());
+//		ObjectMapper mapper = new ObjectMapper();
+//		ContactSearchCriteria criteria = mapper.readValue(getClass().getClassLoader().getResourceAsStream("/etc/searchcriteria/searchcriteria1.json"), ContactSearchCriteria.class);
+//		System.out.println("result"+criteria.getName());
+//
+//
+//
+//		return contact;
+//
+//	}
+//	@Test
+//	public void testContactService() throws JsonParseException, IOException{
+//		Contact contact = readContactFile();
+//
+//	}
+//
+//
+//}
