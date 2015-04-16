c01092bba522176b8e4e6dc90fdaa3b9137d9fbc
Ky Nguyen <kynguyen@kms-technology.com>
2015-04-16 11:19:03 +0700

    [S33] - Update default serialVersionUID

--- src/main/java/vn/kms/launch/contactmgr/domain/greeting/Greeting.java
+++ src/main/java/vn/kms/launch/contactmgr/domain/greeting/Greeting.java
@@ -11,7 +11,9 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Entity
 @Table(name = "GREETINGS")
 public class Greeting extends vn.kms.launch.contactmgr.domain.Entity {
-    @NotEmpty(message = "{validation.not-empty.message}")
+	private static final long serialVersionUID = 1L;
+
+	@NotEmpty(message = "{validation.not-empty.message}")
     @Pattern(regexp = "(\\[A-Za-z]{2})", message = "{validation.iso-3166-code.message}")
     @Column(name = "CODE", unique = true)
     private String code;
