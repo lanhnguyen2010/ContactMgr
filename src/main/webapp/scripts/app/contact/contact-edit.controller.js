30e2e8c886498726025f6f41f4b61bab04c99902
Trieu Doan <trieudoan@kms-technology.com>
2015-04-17 16:51:46 +0700

    [S11] Update UI

--- src/main/webapp/scripts/app/contact/contact-edit.controller.js
+++ src/main/webapp/scripts/app/contact/contact-edit.controller.js
@@ -13,6 +13,8 @@ angular.module('contactmgrApp')
         $scope.selectPhoto = function(imageURL) {
             var avatar = document.getElementById(typePhoto + contactId);
             avatar.src = "../../../photos/" + imageURL; // address of img
+            var avatar2 = document.getElementById(typePhoto + contactId + '_2');
+            avatar2.src = "../../../photos/" + imageURL; // address of img
             document.getElementById('closeButton').click();
         };
 
