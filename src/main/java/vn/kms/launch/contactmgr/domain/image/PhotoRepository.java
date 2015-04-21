56d953698b62b447bbfbdae7707c66a68e03e227
Ky Nguyen <kynguyen@kms-technology.com>
2015-04-21 12:13:53 +0700

    [S33] - Update Convention

--- src/main/java/vn/kms/launch/contactmgr/domain/image/PhotoRepository.java
+++ src/main/java/vn/kms/launch/contactmgr/domain/image/PhotoRepository.java
@@ -1,6 +1,5 @@
 package vn.kms.launch.contactmgr.domain.image;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
