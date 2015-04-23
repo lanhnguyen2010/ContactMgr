0f2dbb3050a17e6c3733940c5a42869d0474eb74
Trang Bien <user.name>
2015-04-23 18:45:23 +0700

    [S23] change login function

--- src/main/webapp/scripts/app/authen/login.controller.js
+++ src/main/webapp/scripts/app/authen/login.controller.js
@@ -1,21 +1,24 @@
 'use strict';
 
 angular.module('contactmgrApp').controller(
-        'LoginController',function($rootScope, $scope, $http, $location) {
+        'LoginController',
+        function($scope,LoginService) {
+            $scope.credentials = {
+                    username: '',
+                    password:''
+            };
+            $scope.para = btoa($scope.credentials.username + ":" + $scope.credentials.password);
+            console.log($scope.para);
+            LoginService.login($scope.para).success(function(data) {
+                console.log($scope.para);
+                if (data.name) {
+                    console.log('success');
+                    $scope.authenticated = false;
+                } else {
+                    console.log('success 11111');
+                }
+            }).error(function() {
+                $scope.authenticated = false;
+            });
             
-            var authenticate = function(credentials, callback) {
-                var headers = credentials ? {
-                    authorization : "Basic "
-                            + btoa(credentials.username + ":"
-                                    + credentials.password)
-                } : {};
-                console.log($scope.credentials.username);
-            };
-            $scope.notice = 'Your registered email';
-            $scope.resetEmail = function(){
-                $scope.email=''
-            };
-            $scope.checkLogin=function(){
-                console.log($scope.credentials.username)
-            }
         });
--- src/main/webapp/scripts/app/authen/login.service.js
+++ src/main/webapp/scripts/app/authen/login.service.js
@@ -1,11 +1,7 @@
 'use strict';
 
-angular.module('contactmgrApp')
-    .service('LoginService', function($http) {
-    	this.login = function (paraLogin) {
-    		return $http.post("/api/authen/login",paraLogin);
-        };
-        this.checkEmail=function(email){
-            return $http.get("/api/authen/checkmail",email);
+angular.module('contactmgrApp').service('LoginService', function($http) {
+    this.login = function (para) {
+            return $http.post("/api/authenticate",para);
         };
     });
