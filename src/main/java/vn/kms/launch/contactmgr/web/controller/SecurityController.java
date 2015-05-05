234666949bb65d99e950ec8502ff46b43ff77e88
Trieu Doan <trieudoan@kms-technology.com>
2015-05-05 11:02:41 +0700

    [S30] Update getUserInfo method on SecurityController

--- src/main/java/vn/kms/launch/contactmgr/web/controller/SecurityController.java
+++ src/main/java/vn/kms/launch/contactmgr/web/controller/SecurityController.java
@@ -4,24 +4,31 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.kms.launch.contactmgr.domain.security.UserInfo;
+import vn.kms.launch.contactmgr.domain.user.User;
+import vn.kms.launch.contactmgr.domain.user.UserRepository;
 import vn.kms.launch.contactmgr.util.SecurityUtil;
 
 @RestController
-
 @RequestMapping(value = "/api/security")
 public class SecurityController {
-
+    @Autowired
+    UserRepository userRepos;
 
     @RequestMapping(value = "/current-user", method = GET)
     @PreAuthorize("hasAnyRole('ADMINISTRATOR', 'DESIGNER', 'EDITOR')")
     public ResponseEntity<UserInfo> getCurrentUser() {
-        UserInfo userInfo = SecurityUtil.getUserInfo();
+        UserInfo userInfo = null;
+        User user = userRepos.findOne(SecurityUtil.getCurrentUserId());
+        if(user != null){
+            userInfo = new UserInfo(user);
+        }
         return new ResponseEntity<>(userInfo, (userInfo == null) ? NOT_FOUND : OK);
     }
 }
